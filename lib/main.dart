b55b90880e964b861976845c53002647502432d3
sajibsaha4647 <sksaha4648@gmail.com>
2022-07-16 01:04:37 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -4,24 +4,47 @@ void main() {
   runApp(const MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return HomePage();
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
-
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp( )
+    return SafeArea(
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(onPressed: (){},child: Icon(Icons.add),),
+        appBar: AppBar(
+          title: const Text("Position"),
+          centerTitle: true,
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          child: Stack(
+            clipBehavior: Clip.none,
+            alignment: Alignment.bottomCenter,
+            children: [
+              Container(
+                height: 100,
+                width: 100,
+                alignment: Alignment.center,
+                color: Colors.amberAccent,
+              ),
+              const Positioned(
+                top: 90,
+                child: Text("sajib"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
-
-
